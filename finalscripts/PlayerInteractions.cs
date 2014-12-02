6ccffb38d8358214db487ed155c616dbc433b4ec
Peter <peterju35@gmail.com>
2014-12-02 15:32:58 -0500

    Update PlayerInteractions.cs

--- finalscripts/PlayerInteractions.cs
+++ finalscripts/PlayerInteractions.cs
@@ -11,17 +11,20 @@ public class PlayerInteractions : MonoBehaviour {
 	public GUIText healthText;
 	private int health;
 
+	// Initialize player health and display it
 	void Start(){
 		health = 20;
 		healthText.text = "Health: " + health.ToString ();
 	}
 
+	// Instantiate a defender in front of the player
 	void PlaceDefender (){
 		Vector3 playerPos = player.transform.position + player.transform.forward;
 		Instantiate (defender, playerPos, Quaternion.identity);
 
 	}
 	
+	// If creep collides with player, player loses health and creep is destroyed
 	void OnTriggerEnter (Collider other){
 		if (other.gameObject.name == "Creep(Clone)"){
 			health = health - 1;
@@ -30,6 +33,8 @@ public class PlayerInteractions : MonoBehaviour {
 		}
 	}
 	
+	// If player presses "Place", a defender is placed in front of him
+	// If player runs out of health, quit the application
 	void Update () {
 		if (Input.GetButton ("Place")){
 			PlaceDefender();
